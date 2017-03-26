3d53d9518a6b3f3d5f947fbdec48eb66444c7469
Will Chertoff <willchertoff@gmail.com>
2017-03-26 16:22:05 -0700

    [FEAT] Page and refrence search v1

---
+++ src/components/MobileHeader/MobileHeader.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+class MobileHeader extends Component {
+  render() {
+    return (
+      <div>
+        Mobile Header
+      </div>
+    )
+  }
+}
+
+export default MobileHeader;
