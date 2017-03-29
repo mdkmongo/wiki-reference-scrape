d43d4c89f4a308ad15c224a53d4c5b66f3ed2ba5
Will Chertoff <willchertoff@gmail.com>
2017-03-28 19:30:51 -0700

    [FEAT] Add filter to references

--- src/routes/references/References.js
+++ src/routes/references/References.js
@@ -1,32 +1,58 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './References.scss';
 
 const noSpecialChars = str => str.replace(/[^a-zA-Z ]/g, " ");
 const noQuotes = str => str.replace(/['"]+/g, '');
 
-function References({references, page}) {
-  return (
-    <div className={s.root}>
-      <div className={s.container}>
-        <h1>{noSpecialChars(page)}</h1>
-        <h4>References:</h4>
-        <div>
-          {
-            references.map((r, i) => {
-              return (
-                <div
-                  key={i}
-                >
-                  <p><a href={r.link} target='_blank' >{r.title}</a></p>
-                </div>
-              )
-            })
-          }
+class References extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sortByRecent: false,
+    }
+  }
+  toggleSortBy = (event) => {
+    event.preventDefault();
+    this.setState({
+      sortByRecent: !this.state.sortByRecent,
+    })
+  }
+  render() {
+    const { references, page } = this.props;
+    const { sortByRecent } = this.state;
+    const filteredRefs = sortByRecent ? references.slice().reverse() : references;
+
+    return (
+      <div className={s.root}>
+        <div className={s.container}>
+          <h1>{noSpecialChars(page)}</h1>
+          <h4>References:</h4>
+          <div className={s.filter}>
+            <p>Sort By:</p>
+            <a href="#" onClick={this.toggleSortBy} >
+              <span className={!sortByRecent ? s.active : ''}>Earliest</span>
+              <span>|</span>
+              <span className={sortByRecent ? s.active : ''}>Most Recent</span>
+            </a>
+          </div>
+          <div>
+            {
+              filteredRefs.map((r, i) => {
+                return (
+                  <div
+                    className={i}
+                  >
+                    <p><a href={r.link} target='_blank' >{r.title}</a></p>
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 References.propTypes = {
